c2f9493caef61d5d9926d14b84561f29fe286607
zhangchao <529743595@qq.com>
2019-12-04 21:43:08 +0800

    绑定原型

--- test/index.js
+++ test/index.js
@@ -11,4 +11,10 @@ describe('myBind', () => {
   it('是一个函数', () => {
     assert.isFunction(myBind)
   })
+  it('绑定在函数的原型上', () => {
+    const fn = function() {
+      return this.name
+    }
+    assert.isFunction(fn.myBind)
+  })
 })
